6dfb01ebbd6576996d0d0f2f571bdf3b9180b91f
Tyler <tyler.rinker@gmail.com>
2017-01-30 21:16:19 -0500

    * `read_dir_transcript` added to complement `read-dir` aimed at a directory of   transcripts.
    close #4

---
+++ R/read_dir_transcript.R
@@ -0,0 +1,123 @@
+#' Read In Multiple Transcript Files From a Directory
+#'
+#' Read in multiple transcript files from a directory and create a
+#' \code{\link[base]{data.frame}}.
+#'
+#' @param path Path to the directory.
+#' @param col.names  A character vector specifying the column names of the
+#' transcript columns (document, person, dialogue).
+#' @param pattern An optional regular expression. Only file names which match
+#' the regular expression will be returned.
+#' @param all.files Logical.   If \code{FALSE}, only the names of visible files
+#' are returned. If \code{TRUE}, all file names will be returned.
+#' @param recursive Logical. Should the listing recurse into directories?
+#' @param skip Integer; the number of lines of the data file to skip before
+#' beginning to read data.
+#' @param merge.broke.tot logical.  If \code{TRUE} and if the file being read in
+#' is .docx with broken space between a single turn of talk read_transcript
+#' will attempt to merge these into a single turn of talk.
+#' @param header logical.  If \code{TRUE} the file contains the names of the
+#' variables as its first line.
+#' @param dash A character string to replace the en and em dashes special
+#' characters (default is to remove).
+#' @param ellipsis A character string to replace the ellipsis special characters.
+#' @param quote2bracket logical. If \code{TRUE} replaces curly quotes with curly
+#' braces (default is \code{FALSE}).  If \code{FALSE} curly quotes are removed.
+#' @param rm.empty.rows logical.  If \code{TRUE}
+#' \code{\link[textreadr]{read_transcript}}  attempts to remove empty rows.
+#' @param na A character string to be interpreted as an \code{NA} value.
+#' @param sep The field separator character. Values on each line of the file are
+#' separated by this character.  The default of \code{NULL} instructs
+#' \code{\link[textreadr]{read_transcript}} to use a separator suitable for the file
+#' type being read in.
+#' @param comment.char A character vector of length one containing a single
+#' character or an empty string. Use \code{""} to turn off the interpretation of
+#' comments altogether.
+#' @param max.person.nchar The max number of characters long names are expected
+#' to be.  This information is used to warn the user if a separat appears beyond
+#' this length in the text.
+#' @param \ldots ignored.
+#' @return Returns a dataframe of documents, dialogue, and people.
+#' @export
+#' @seealso read_transcript
+#' @examples
+#' skips <- c(0, 1, 1, 0, 0, 1)
+#' path <- system.file("docs/transcripts", package = 'textreadr')
+#' textreadr::peek(read_dir_transcript(path, skip = skips), Inf)
+#'
+#' \dontrun{
+#' ## with additional  cleaning
+#' library(tidyverse, textshape, textclean)
+#'
+#' path %>%
+#'     read_dir_transcript(skip = skips) %>%
+#'     textclean::filter_row("Person", "^\\[") %>%
+#'     mutate(
+#'         Person = stringi::stri_replace_all_regex(Person, "(^/\\s*)|(:\\s*$)", "") %>%
+#'             trimws(),
+#'         Dialogue = stringi::stri_replace_all_regex(Dialogue, "(^/\\s*)", "")
+#'     ) %>%
+#'     peek(Inf)
+#' }
+read_dir_transcript <- function(path, col.names = c("Document", "Person", "Dialogue"),
+    pattern = NULL, all.files = FALSE,
+    recursive = FALSE, skip = 0, merge.broke.tot = TRUE, header = FALSE, dash = "", ellipsis = "...",
+    quote2bracket = FALSE, rm.empty.rows = TRUE, na = "", sep = NULL,
+    comment.char = "", max.person.nchar = 20, ...) {
+
+    to_read_in <- list_files(path, all.files = all.files, full.names = TRUE, recursive = recursive)
+    if (identical(character(0), to_read_in)) {
+        stop("The following location does not appear to contain files:\n   -", path)
+    }
+
+    if (length(skip) == 1) skip <- rep(skip, length(to_read_in))
+    if (length(merge.broke.tot) == 1) merge.broke.tot <- rep(merge.broke.tot, length(to_read_in))
+    if (length(header) == 1) header<- rep(header, length(to_read_in))
+    if (length(dash) == 1) dash <- rep(dash, length(to_read_in))
+    if (length(ellipsis) == 1) ellipsis <- rep(ellipsis, length(to_read_in))
+    if (length(quote2bracket) == 1) quote2bracket <- rep(quote2bracket, length(to_read_in))
+    if (length(rm.empty.rows) == 1) rm.empty.rows <- rep(rm.empty.rows, length(to_read_in))
+    if (length(na) == 1) na <- rep(na, length(to_read_in))
+    if (is.null(sep)) sep <- lapply(seq_along(to_read_in), function(i) NULL)
+    if (length(sep) == 1) sep <- rep(sep, length(to_read_in))
+    if (length(comment.char) == 1) comment.char <- rep(comment.char, length(to_read_in))
+    if (length(max.person.nchar) == 1) max.person.nchar <- rep(max.person.nchar, length(to_read_in))
+
+    arg_list <- list(skip, merge.broke.tot, header, dash, ellipsis, quote2bracket,
+        rm.empty.rows, na, sep, comment.char, max.person.nchar
+    )
+
+    arg_nms <- c('skip', 'merge.broke.tot', 'header', 'dash', 'ellipsis', 'quote2bracket',
+        'rm.empty.rows', 'na', 'sep', 'comment.char', 'max.person.nchar'
+    )
+
+    lapply(seq_along(arg_list), function(i){
+
+        if(length(arg_list[[i]]) != length(to_read_in)) stop(paste0('`', arg_nms[i], '` is not of length 1 or the same length as files in `path`'))
+    })
+
+    #paste(c('skip', 'merge.broke.tot', 'header', 'dash', 'ellipsis',
+    # 'quote2bracket', 'rm.empty.rows', 'na', 'sep',
+    # 'comment.char', 'max.person.nchar'), collapse=", ")
+
+    reads <- Map(function(x, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10, y11) {
+        try(read_transcript(x, col.names = col.names[-1], skip = y1,
+            merge.broke.tot = y2, header = y3, dash = y4, ellipsis = y5,
+            quote2bracket = y6, rm.empty.rows = y7, na = y8, sep = y9,
+            comment.char = y10, max.person.nchar = y11, ...))
+    }, to_read_in, skip, merge.broke.tot, header, dash, ellipsis,
+       quote2bracket, rm.empty.rows, na, sep, comment.char, max.person.nchar)
+
+    names(reads) <- tools::file_path_sans_ext(basename(to_read_in))
+
+    goods <- !sapply(reads, inherits, 'try-error')
+    if (any(!goods)) {
+        warning(paste0("The following files did not read in correctly:\n",
+            paste0('  - ', to_read_in[!goods], collapse = "\n")
+        ))
+    }
+    textshape::tidy_list(reads[goods], col.names[1])
+
+}
+
+
--- README.md
+++ README.md
@@ -63,26 +63,26 @@ XML, .html, and SQL. For these first 4 forms the
 <table>
 <thead>
 <tr class="header">
-<th align="left">R Package</th>
-<th align="left">SQL</th>
+<th>R Package</th>
+<th>SQL</th>
 </tr>
 </thead>
 <tbody>
 <tr class="odd">
-<td align="left">ROBDC</td>
-<td align="left">Microsoft SQL Server</td>
+<td>ROBDC</td>
+<td>Microsoft SQL Server</td>
 </tr>
 <tr class="even">
-<td align="left">RMySQL</td>
-<td align="left">MySQL</td>
+<td>RMySQL</td>
+<td>MySQL</td>
 </tr>
 <tr class="odd">
-<td align="left">ROracle</td>
-<td align="left">Oracle</td>
+<td>ROracle</td>
+<td>Oracle</td>
 </tr>
 <tr class="even">
-<td align="left">RJDBC</td>
-<td align="left">JDBC</td>
+<td>RJDBC</td>
+<td>JDBC</td>
 </tr>
 </tbody>
 </table>
@@ -97,51 +97,56 @@ table below:
 <table>
 <thead>
 <tr class="header">
-<th align="left">Function</th>
-<th align="left">Task</th>
-<th align="left">Description</th>
+<th>Function</th>
+<th>Task</th>
+<th>Description</th>
 </tr>
 </thead>
 <tbody>
 <tr class="odd">
-<td align="left"><code>read_transcript</code></td>
-<td align="left">reading</td>
-<td align="left">Read 2 column transcripts</td>
+<td><code>read_transcript</code></td>
+<td>reading</td>
+<td>Read 2 column transcripts</td>
 </tr>
 <tr class="even">
-<td align="left"><code>read_docx</code></td>
-<td align="left">reading</td>
-<td align="left">Read .docx</td>
+<td><code>read_docx</code></td>
+<td>reading</td>
+<td>Read .docx</td>
 </tr>
 <tr class="odd">
-<td align="left"><code>read_doc</code></td>
-<td align="left">reading</td>
-<td align="left">Read .doc</td>
+<td><code>read_doc</code></td>
+<td>reading</td>
+<td>Read .doc</td>
 </tr>
 <tr class="even">
-<td align="left"><code>read_document</code></td>
-<td align="left">reading</td>
-<td align="left">Generic text reader for .doc, .docx, .txt, .pdf</td>
+<td><code>read_document</code></td>
+<td>reading</td>
+<td>Generic text reader for .doc, .docx, .txt, .pdf</td>
 </tr>
 <tr class="odd">
-<td align="left"><code>read_pdf</code></td>
-<td align="left">reading</td>
-<td align="left">Read .pdf</td>
+<td><code>read_pdf</code></td>
+<td>reading</td>
+<td>Read .pdf</td>
 </tr>
 <tr class="even">
-<td align="left"><code>read_dir</code></td>
-<td align="left">reading</td>
-<td align="left">Read and format multiple .txt files</td>
+<td><code>read_dir</code></td>
+<td>reading</td>
+<td>Read and format multiple .doc, .docx, .txt, .pdf files</td>
 </tr>
 <tr class="odd">
-<td align="left"><code>download</code></td>
-<td align="left">downloading</td>
-<td align="left">Download documents</td>
+<td><code>read_dir_transcript</code></td>
+<td>reading</td>
+<td>Read and format multiple transcript files</td>
 </tr>
 <tr class="even">
-<td align="left"><code>peek</code></td>
-<td align="left">viewing</td>
-<td align="left">Truncated viewing of <code>data.frame</code>s</td>
+<td><code>download</code></td>
+<td>downloading</td>
+<td>Download documents</td>
+</tr>
+<tr class="odd">
+<td><code>peek</code></td>
+<td>viewing</td>
+<td>Truncated viewing of <code>data.frame</code>s</td>
 </tr>
 </tbody>
 </table>
@@ -204,7 +209,7 @@ Here I download a .docx file of presidential debated from 2012.
         read_docx() %>%
         head(3)
 
-    ## pres.deb1.docx read into C:\Users\Tyler\AppData\Local\Temp\RtmpmGqxDP
+    ## pres.deb1.docx read into C:\Users\Tyler\AppData\Local\Temp\RtmpUF08U5
 
     ## [1] "LEHRER: We'll talk about -- specifically about health care in a moment. But what -- do you support the voucher system, Governor?"                           
     ## [2] "ROMNEY: What I support is no change for current retirees and near-retirees to Medicare. And the president supports taking $716 billion out of that program."
@@ -443,7 +448,7 @@ handles. These are the files that will be read in:
     basename(trans_docs)
 
     ## [1] "trans1.docx" "trans2.docx" "trans3.docx" "trans4.xlsx" "trans5.xls" 
-    ## [6] "trans6.doc"
+    ## [6] "trans6.doc"  "transcripts"
 
 ### docx Simple
 
@@ -624,7 +629,7 @@ I demonstrate pairings with
         textshape::split_index(which(.$loc) -1) %>%
         lapply(select, -loc)
 
-    ## SCDB_2012_01_codebook.pdf read into C:\Users\Tyler\AppData\Local\Temp\RtmpmGqxDP
+    ## SCDB_2012_01_codebook.pdf read into C:\Users\Tyler\AppData\Local\Temp\RtmpUF08U5
 
     ## Function to extract cases
     ex_vs <- qdapRegex::ex_(pattern = "((of|[A-Z][A-Za-z'.,-]+)\\s+)+([Vv]s?\\.\\s+)(([A-Z][A-Za-z'.,-]+\\s+)*((of|[A-Z][A-Za-z',.-]+),?($|\\s+|\\d))+)")
