b0060571c525492e8050d1f7b5495bbef9a549d7
Tyler Rinker <trinker@campuslabs.com>
2020-04-22 11:43:03 -0400

    read_pptx now can include notes

--- R/read_pptx.R
+++ R/read_pptx.R
@@ -8,6 +8,7 @@
 #' removed.
 #' @param trim logical.  If \code{TRUE} the leading/training white space is
 #' removed.
+#' @param include.notes logical.  If \code{TRUE} then slide notes are included.
 #' @param \dots ignored.
 #' @return Returns a \code{\link[base]{data.frame}} with the slide number
 #' (\code{slide_id}), line number (\code{element_id}), and the \code{text}.
@@ -18,8 +19,12 @@
 #' url <- "https://www.oclc.org/content/dam/research/presentations/2019/111319-godby-NISO-What-Are-Entities-Matter.pptx"
 #' file <- download(url)
 #' (txt <- read_pptx(file))
+#' 
+#' pptx_doc <- system.file('docs/Hello_World.pptx', package = "textreadr")
+#' read_pptx(pptx_doc)
+#' read_pptx(pptx_doc, include.notes = TRUE)
 #' }
-read_pptx <- function (file, skip = 0, remove.empty = TRUE, trim = TRUE, ...) {
+read_pptx <- function (file, skip = 0, remove.empty = TRUE, trim = TRUE, include.notes = FALSE, ...) {
 
     filetype <- tools::file_ext(file)
     if (filetype %in% c('pptx') && grepl('^([fh]ttp)', file)){
@@ -37,17 +42,62 @@ read_pptx <- function (file, skip = 0, remove.empty = TRUE, trim = TRUE, ...) {
 
     ## unzip docx
     utils::unzip(file, exdir = tmp)
-    xmlfiles <- dir(file.path(tmp, "ppt/slides"), pattern = '[.]xml$', full.names = TRUE)
+
+    slides <- extract_pptx_nodes(
+        tmp,
+        trim = trim,
+        remove.empty = remove.empty, 
+        skip = skip
+    )
+    
+    if (include.notes){
+
+        notes <- extract_pptx_nodes(
+            tmp, 
+            dir = 'notesSlides', 
+            tag1 = '//p:txBody//a:p', 
+            starts = lapply(slides, nrow),
+            trim = trim,
+            remove.empty = remove.empty,
+            skip = 0
+        )
+     
+        slides <- lapply(names(slides), function(i){  
+
+            rbind(
+                slides[[i]],
+                notes[[i]],
+                stringsAsFactors = FALSE
+            )
+
+        })
+    }
+
+    textshape::tidy_list(slides, 'slide_id')
+
+}
+
+
+extract_pptx_nodes <- function(loc, dir = 'slides', tag1 = '//a:p', tag2 = 'a:t', starts = NULL, remove.empty, trim, skip){
+
+    xmlfiles <- dir(file.path(loc, "ppt", dir), pattern = '[.]xml$', full.names = TRUE)
 
     ## read in the unzipped docx
     docs <- lapply(xmlfiles, xml2::read_xml)
 
-    out <- lapply(docs, function(doc){
-        ## extract the content
-        children <- lapply(xml2::xml_find_all(doc, '//a:p'), xml2::xml_children)
-        pvalues <- unlist(lapply(children, function(x) {
-            paste(xml2::xml_text(xml2::xml_find_all(x, 'a:t')), collapse = ' ')
-        }))
+    if (is.null(starts)) starts <- as.list(rep(0, length(docs)))
+
+    out <- Map(function(doc){
+
+        if (dir == 'slides'){
+            ## extract the content
+            children <- lapply(xml2::xml_find_all(doc, tag1), xml2::xml_children)
+            pvalues <- unlist(lapply(children, function(x) {
+                paste(xml2::xml_text(xml2::xml_find_all(x, tag2)), collapse = ' ')
+            }))
+        } else {
+            pvalues <- lapply(xml2::xml_find_all(doc, tag1), xml2::xml_text)
+        }
  
         ## formatting
         if (isTRUE(remove.empty)) pvalues <- pvalues[!grepl("^\\s*$", pvalues)]
@@ -55,9 +105,23 @@ read_pptx <- function (file, skip = 0, remove.empty = TRUE, trim = TRUE, ...) {
         if (isTRUE(trim)) pvalues <- trimws(pvalues)
         data.frame(element_id = seq_along(pvalues), text = pvalues, stringsAsFactors = FALSE)
 
-    })
+    }, docs)
 
-    
-    textshape::tidy_list(out, 'slide_id')
+    if (dir == 'slides'){
+
+        names(out) <- gsub('^(notes)?[Ss]lide|\\.xml$', '', basename(xmlfiles))
+
+    } else {
+
+        names(out) <- unlist(lapply(out, function(x) x$text[nrow(x)]))
+
+        lens <- unlist(starts[as.integer(names(out))])
+        out[] <- Map(function(x, len) {
+             x$element_id <- x$element_id + len
+             x[1:(nrow(x) - 1),]
+        }, out, lens)
+    }
+
+    out
 
 }
